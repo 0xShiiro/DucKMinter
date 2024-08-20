c05d66d293f53bb12ef40b79397eaef908df775b
CodeFreakPriyanshu <priyanshusoni0646@gmail.com>
2024-08-21 02:47:33 +0530

    Basic Setup Done

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Fileupload from './Fileupload.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1>NFT Minter</h1>
+        <Fileupload />
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
